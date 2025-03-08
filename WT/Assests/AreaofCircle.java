32a97bf9da80f76c052196ba94624596ab4a7718
ChamaleVanaja <chamalevanaja@gmail.com>
2025-03-08 09:12:58 +0530

    Add files via upload

---
+++ WT/Assests/AreaofCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class AreaofCircle
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius of circle: ");
+		double r = sc.nextDouble();
+		System.out.println("AREA OF  of circle: "+3.14*r*r);
+	}
+}
---
+++ WT/Assests/Product.java
@@ -0,0 +1,12 @@
+class Product 
+{
+	public static void main(String[] args) 
+	{
+		product = 1;
+		for (i=1;i<=5 ;i++ )
+		{
+			pro = pro*i;
+		}
+			System.out.println(pro);
+	}
+}
